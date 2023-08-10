cb9c6959d0a69652040a47369f5e6074fd44f3ef
shushuTona <42406967+shushuTona@users.noreply.github.com>
2023-08-11 02:03:28 +0900

    fixed css

--- blog/components/blog/ArticleList.tsx
+++ blog/components/blog/ArticleList.tsx
@@ -10,6 +10,9 @@ type Article = {
 }
 
 const ArticleList = ( props: { articleList: Article[], headingLevel: string, classList?: string } ) => {
+    const headingClass = props.headingLevel === '2' ? 'text-xl' : 'text-base';
+    const itemClass = props.headingLevel === '2' ? '[&:nth-of-type(n+2)]:pt-8 [&:nth-of-type(n+2)]:mt-8' : '[&:nth-of-type(n+2)]:pt-6 [&:nth-of-type(n+2)]:mt-6';
+    const dateListClass = props.headingLevel === '2' ? 'my-4' : 'my-2';
     const CustomTag = `h${props.headingLevel}` as keyof JSX.IntrinsicElements;
 
     return (
@@ -17,12 +20,12 @@ const ArticleList = ( props: { articleList: Article[], headingLevel: string, cla
             {
                 props.articleList.map( ( { articleMetaData, articlePath } ) => {
                     return (
-                        <li key={articleMetaData.title} className='[&:nth-of-type(n+2)]:pt-8 [&:nth-of-type(n+2)]:mt-8 [&:nth-of-type(n+2)]:border-solid [&:nth-of-type(n+2)]:border-t [&:nth-of-type(n+2)]:border-t-[#350391]'>
-                            <CustomTag className='m-0 text-xl'>
+                        <li key={articleMetaData.title} className={'[&:nth-of-type(n+2)]:border-solid [&:nth-of-type(n+2)]:border-t [&:nth-of-type(n+2)]:border-t-[#350391] ' + itemClass}>
+                            <CustomTag className={'m-0 ' + headingClass}>
                                 <Link href={articlePath}>{articleMetaData.title}</Link>
                             </CustomTag>
 
-                            <DateList created_at={articleMetaData.created_at} updated_at={articleMetaData.updated_at} classList='my-4' />
+                            <DateList created_at={articleMetaData.created_at} updated_at={articleMetaData.updated_at} classList={dateListClass} />
 
                             <TagList tags={articleMetaData.tags} classList='m-0' />
                         </li>
--- blog/components/blog/TagList.tsx
+++ blog/components/blog/TagList.tsx
@@ -2,12 +2,12 @@ import Link from 'next/link';
 
 const TagList = ( props: { tags: string[], classList?: string } ) => {
     return (
-        <ul className={'flex gap-2 ' + props.classList}>
+        <ul className={'flex gap-2 text-xs ' + props.classList}>
             {
                 props.tags.map( ( tag ) => {
                     const href = '/tag/' + tag
                     return <li key={tag}>
-                        <Link className='px-2 py-1 block bg-neutral-300 transition-[background-color] duration-500 hover:bg-neutral-200 text-sm no-underline rounded-[6px]' href={href}>#{tag}</Link>
+                        <Link className='px-2 py-1 block leading-tight bg-neutral-300 transition-[background-color] duration-500 hover:bg-neutral-200 no-underline rounded-[6px]' href={href}>#{tag}</Link>
                     </li>
                 } )
             }

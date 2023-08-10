e410297f304c22998955f4e77380f26e37c874f6
shushuTona <42406967+shushuTona@users.noreply.github.com>
2023-08-10 09:35:21 +0900

    update tag page path

--- blog/components/blog/TagList.tsx
+++ blog/components/blog/TagList.tsx
@@ -5,7 +5,7 @@ const TagList = ( props: { tags: string[], classList?: string } ) => {
         <ul className={'flex gap-2 ' + props.classList}>
             {
                 props.tags.map( ( tag ) => {
-                    const href = '/blog/' + tag
+                    const href = '/blog/tag/' + tag
                     return <li key={tag}>
                         <Link className='px-2 py-1 block bg-neutral-300 transition-[background-color] duration-500 hover:bg-neutral-200 text-sm no-underline rounded-[6px]' href={href}>#{tag}</Link>
                     </li>

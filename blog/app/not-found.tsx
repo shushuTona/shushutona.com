3a1279356bc35ecd7ee8156b80ba2c2d2b78817e
shushuTona <42406967+shushuTona@users.noreply.github.com>
2023-08-13 20:50:02 +0900

    add not found page

---
+++ blog/app/not-found.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+ 
+export default async function NotFound() {
+  return (
+    <>
+      <h1>Not Found</h1>
+      <p>Could not find requested resource</p>
+      <p>View <Link href="/">All posts</Link></p>
+    </>
+  )
+}
